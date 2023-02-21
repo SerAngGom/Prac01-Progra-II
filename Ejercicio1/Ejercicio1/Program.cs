fa0130540be2fee75d5650d1df038219483033b0
SerAngGom <al405280@uji.es>
2023-02-21 18:25:44 +0100

    Ejercicio 1

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -10,6 +10,14 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("¿Cuantos paquetes de folios desea comprar?");
+            int cantidad=Convert.ToInt32(Console.ReadLine());
+            double precio;
+            if (cantidad < 200) precio = cantidad * 4.50;
+            else if (cantidad >= 200 && cantidad <= 500) precio = cantidad * 3.75;
+            else precio = cantidad * 3.25;
+            Console.WriteLine("El precio a pagar por los "+ cantidad+ " paquetes es de "+precio + " euros.");
+            Console.ReadKey();
         }
     }
 }
