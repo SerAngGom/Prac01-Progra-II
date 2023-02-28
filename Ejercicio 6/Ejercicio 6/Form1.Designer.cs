a057e66ca81823648a8e70d118479cedb604bc43
SerAngGom <al405280@uji.es>
2023-02-28 19:24:52 +0100

    Ejercicio 6 en proceso
    Arreglar los operadores antes de igual

--- Ejercicio 6/Ejercicio 6/Form1.Designer.cs
+++ Ejercicio 6/Ejercicio 6/Form1.Designer.cs
@@ -28,13 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tecla1 = new System.Windows.Forms.Button();
+            this.tecla3 = new System.Windows.Forms.Button();
+            this.tecla4 = new System.Windows.Forms.Button();
+            this.tecla5 = new System.Windows.Forms.Button();
+            this.tecla6 = new System.Windows.Forms.Button();
+            this.tecla7 = new System.Windows.Forms.Button();
+            this.tecla8 = new System.Windows.Forms.Button();
+            this.tecla9 = new System.Windows.Forms.Button();
+            this.teclamas = new System.Windows.Forms.Button();
+            this.teclamenos = new System.Windows.Forms.Button();
+            this.teclamult = new System.Windows.Forms.Button();
+            this.tecla0 = new System.Windows.Forms.Button();
+            this.teclapunto = new System.Windows.Forms.Button();
+            this.teclacambio = new System.Windows.Forms.Button();
+            this.tecladiv = new System.Windows.Forms.Button();
+            this.teclaC = new System.Windows.Forms.Button();
+            this.teclaCE = new System.Windows.Forms.Button();
+            this.teclaigual = new System.Windows.Forms.Button();
+            this.tecla2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tecla1
+            // 
+            this.tecla1.Location = new System.Drawing.Point(62, 115);
+            this.tecla1.Name = "tecla1";
+            this.tecla1.Size = new System.Drawing.Size(93, 62);
+            this.tecla1.TabIndex = 0;
+            this.tecla1.Text = "1";
+            this.tecla1.UseVisualStyleBackColor = true;
+            this.tecla1.Click += new System.EventHandler(this.tecla1_Click);
+            // 
+            // tecla3
+            // 
+            this.tecla3.Location = new System.Drawing.Point(260, 115);
+            this.tecla3.Name = "tecla3";
+            this.tecla3.Size = new System.Drawing.Size(93, 62);
+            this.tecla3.TabIndex = 2;
+            this.tecla3.Text = "3";
+            this.tecla3.UseVisualStyleBackColor = true;
+            this.tecla3.Click += new System.EventHandler(this.tecla3_Click);
+            // 
+            // tecla4
+            // 
+            this.tecla4.Location = new System.Drawing.Point(62, 183);
+            this.tecla4.Name = "tecla4";
+            this.tecla4.Size = new System.Drawing.Size(93, 62);
+            this.tecla4.TabIndex = 3;
+            this.tecla4.Text = "4";
+            this.tecla4.UseVisualStyleBackColor = true;
+            this.tecla4.Click += new System.EventHandler(this.tecla4_Click);
+            // 
+            // tecla5
+            // 
+            this.tecla5.Location = new System.Drawing.Point(161, 183);
+            this.tecla5.Name = "tecla5";
+            this.tecla5.Size = new System.Drawing.Size(93, 62);
+            this.tecla5.TabIndex = 4;
+            this.tecla5.Text = "5";
+            this.tecla5.UseVisualStyleBackColor = true;
+            this.tecla5.Click += new System.EventHandler(this.tecla5_Click);
+            // 
+            // tecla6
+            // 
+            this.tecla6.Location = new System.Drawing.Point(260, 183);
+            this.tecla6.Name = "tecla6";
+            this.tecla6.Size = new System.Drawing.Size(93, 62);
+            this.tecla6.TabIndex = 5;
+            this.tecla6.Text = "6";
+            this.tecla6.UseVisualStyleBackColor = true;
+            this.tecla6.Click += new System.EventHandler(this.tecla6_Click);
+            // 
+            // tecla7
+            // 
+            this.tecla7.Location = new System.Drawing.Point(62, 251);
+            this.tecla7.Name = "tecla7";
+            this.tecla7.Size = new System.Drawing.Size(93, 62);
+            this.tecla7.TabIndex = 6;
+            this.tecla7.Text = "7";
+            this.tecla7.UseVisualStyleBackColor = true;
+            this.tecla7.Click += new System.EventHandler(this.tecla7_Click);
+            // 
+            // tecla8
+            // 
+            this.tecla8.Location = new System.Drawing.Point(161, 251);
+            this.tecla8.Name = "tecla8";
+            this.tecla8.Size = new System.Drawing.Size(93, 62);
+            this.tecla8.TabIndex = 7;
+            this.tecla8.Text = "8";
+            this.tecla8.UseVisualStyleBackColor = true;
+            this.tecla8.Click += new System.EventHandler(this.tecla8_Click);
+            // 
+            // tecla9
+            // 
+            this.tecla9.Location = new System.Drawing.Point(260, 251);
+            this.tecla9.Name = "tecla9";
+            this.tecla9.Size = new System.Drawing.Size(93, 62);
+            this.tecla9.TabIndex = 8;
+            this.tecla9.Text = "9";
+            this.tecla9.UseVisualStyleBackColor = true;
+            this.tecla9.Click += new System.EventHandler(this.tecla9_Click);
+            // 
+            // teclamas
+            // 
+            this.teclamas.Location = new System.Drawing.Point(359, 115);
+            this.teclamas.Name = "teclamas";
+            this.teclamas.Size = new System.Drawing.Size(93, 62);
+            this.teclamas.TabIndex = 9;
+            this.teclamas.Text = "+";
+            this.teclamas.UseVisualStyleBackColor = true;
+            this.teclamas.Click += new System.EventHandler(this.teclamas_Click);
+            // 
+            // teclamenos
+            // 
+            this.teclamenos.Location = new System.Drawing.Point(359, 183);
+            this.teclamenos.Name = "teclamenos";
+            this.teclamenos.Size = new System.Drawing.Size(93, 62);
+            this.teclamenos.TabIndex = 10;
+            this.teclamenos.Text = "-";
+            this.teclamenos.UseVisualStyleBackColor = true;
+            this.teclamenos.Click += new System.EventHandler(this.teclamenos_Click);
+            // 
+            // teclamult
+            // 
+            this.teclamult.Location = new System.Drawing.Point(359, 251);
+            this.teclamult.Name = "teclamult";
+            this.teclamult.Size = new System.Drawing.Size(93, 62);
+            this.teclamult.TabIndex = 11;
+            this.teclamult.Text = "*";
+            this.teclamult.UseVisualStyleBackColor = true;
+            this.teclamult.Click += new System.EventHandler(this.teclamult_Click);
+            // 
+            // tecla0
+            // 
+            this.tecla0.Location = new System.Drawing.Point(62, 319);
+            this.tecla0.Name = "tecla0";
+            this.tecla0.Size = new System.Drawing.Size(93, 62);
+            this.tecla0.TabIndex = 12;
+            this.tecla0.Text = "0";
+            this.tecla0.UseVisualStyleBackColor = true;
+            this.tecla0.Click += new System.EventHandler(this.tecla0_Click);
+            // 
+            // teclapunto
+            // 
+            this.teclapunto.Location = new System.Drawing.Point(161, 319);
+            this.teclapunto.Name = "teclapunto";
+            this.teclapunto.Size = new System.Drawing.Size(93, 62);
+            this.teclapunto.TabIndex = 13;
+            this.teclapunto.Text = ".";
+            this.teclapunto.UseVisualStyleBackColor = true;
+            this.teclapunto.Click += new System.EventHandler(this.teclapunto_Click);
+            // 
+            // teclacambio
+            // 
+            this.teclacambio.Location = new System.Drawing.Point(260, 319);
+            this.teclacambio.Name = "teclacambio";
+            this.teclacambio.Size = new System.Drawing.Size(93, 62);
+            this.teclacambio.TabIndex = 14;
+            this.teclacambio.Text = "+\r\n-";
+            this.teclacambio.UseVisualStyleBackColor = true;
+            this.teclacambio.Click += new System.EventHandler(this.teclacambio_Click);
+            // 
+            // tecladiv
+            // 
+            this.tecladiv.Location = new System.Drawing.Point(359, 319);
+            this.tecladiv.Name = "tecladiv";
+            this.tecladiv.Size = new System.Drawing.Size(93, 62);
+            this.tecladiv.TabIndex = 15;
+            this.tecladiv.Text = "/";
+            this.tecladiv.UseVisualStyleBackColor = true;
+            this.tecladiv.Click += new System.EventHandler(this.tecladiv_Click);
+            // 
+            // teclaC
+            // 
+            this.teclaC.Location = new System.Drawing.Point(458, 115);
+            this.teclaC.Name = "teclaC";
+            this.teclaC.Size = new System.Drawing.Size(93, 62);
+            this.teclaC.TabIndex = 16;
+            this.teclaC.Text = "C";
+            this.teclaC.UseVisualStyleBackColor = true;
+            // 
+            // teclaCE
+            // 
+            this.teclaCE.Location = new System.Drawing.Point(458, 183);
+            this.teclaCE.Name = "teclaCE";
+            this.teclaCE.Size = new System.Drawing.Size(93, 62);
+            this.teclaCE.TabIndex = 17;
+            this.teclaCE.Text = "CE";
+            this.teclaCE.UseVisualStyleBackColor = true;
+            this.teclaCE.Click += new System.EventHandler(this.teclaCE_Click);
+            // 
+            // teclaigual
+            // 
+            this.teclaigual.Location = new System.Drawing.Point(458, 251);
+            this.teclaigual.Name = "teclaigual";
+            this.teclaigual.Size = new System.Drawing.Size(93, 130);
+            this.teclaigual.TabIndex = 18;
+            this.teclaigual.Text = "=";
+            this.teclaigual.UseVisualStyleBackColor = true;
+            this.teclaigual.Click += new System.EventHandler(this.teclaigual_Click);
+            // 
+            // tecla2
+            // 
+            this.tecla2.Location = new System.Drawing.Point(161, 115);
+            this.tecla2.Name = "tecla2";
+            this.tecla2.Size = new System.Drawing.Size(93, 62);
+            this.tecla2.TabIndex = 20;
+            this.tecla2.Text = "2";
+            this.tecla2.UseVisualStyleBackColor = true;
+            this.tecla2.Click += new System.EventHandler(this.tecla2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 26);
+            this.textBox1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tecla2);
+            this.Controls.Add(this.teclaigual);
+            this.Controls.Add(this.teclaCE);
+            this.Controls.Add(this.teclaC);
+            this.Controls.Add(this.tecladiv);
+            this.Controls.Add(this.teclacambio);
+            this.Controls.Add(this.teclapunto);
+            this.Controls.Add(this.tecla0);
+            this.Controls.Add(this.teclamult);
+            this.Controls.Add(this.teclamenos);
+            this.Controls.Add(this.teclamas);
+            this.Controls.Add(this.tecla9);
+            this.Controls.Add(this.tecla8);
+            this.Controls.Add(this.tecla7);
+            this.Controls.Add(this.tecla6);
+            this.Controls.Add(this.tecla5);
+            this.Controls.Add(this.tecla4);
+            this.Controls.Add(this.tecla3);
+            this.Controls.Add(this.tecla1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button tecla1;
+        private System.Windows.Forms.Button tecla3;
+        private System.Windows.Forms.Button tecla4;
+        private System.Windows.Forms.Button tecla5;
+        private System.Windows.Forms.Button tecla6;
+        private System.Windows.Forms.Button tecla7;
+        private System.Windows.Forms.Button tecla8;
+        private System.Windows.Forms.Button tecla9;
+        private System.Windows.Forms.Button teclamas;
+        private System.Windows.Forms.Button teclamenos;
+        private System.Windows.Forms.Button teclamult;
+        private System.Windows.Forms.Button tecla0;
+        private System.Windows.Forms.Button teclapunto;
+        private System.Windows.Forms.Button teclacambio;
+        private System.Windows.Forms.Button tecladiv;
+        private System.Windows.Forms.Button teclaC;
+        private System.Windows.Forms.Button teclaCE;
+        private System.Windows.Forms.Button teclaigual;
+        private System.Windows.Forms.Button tecla2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Ejercicio 6/Ejercicio 6/Form1.cs
+++ Ejercicio 6/Ejercicio 6/Form1.cs
@@ -12,9 +12,118 @@ namespace Ejercicio_6
 {
     public partial class Form1 : Form
     {
+        String operacion;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tecla1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void tecla2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void tecla3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void tecla4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void tecla5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void tecla6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void tecla7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void tecla8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void tecla9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void tecla0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void teclapunto_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ".";
+        }
+
+        private void teclacambio_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToDouble(textBox1.Text) > 0) 
+            {
+                textBox1.Text = '-' + textBox1.Text;
+            }
+            else if (Convert.ToDouble(textBox1.Text) < 0)
+            {
+                double n = Convert.ToDouble(textBox1.Text);
+                n *= -1;
+                textBox1.Text = Convert.ToString(n);
+            } 
+        }
+
+        private void teclamas_Click(object sender, EventArgs e)
+        {
+            operacion += textBox1.Text + "+";
+            textBox1.Text = "";
+
+        }
+
+        private void teclamenos_Click(object sender, EventArgs e)
+        {
+            operacion += textBox1.Text + "-";
+            textBox1.Text = "";
+        }
+
+        private void teclamult_Click(object sender, EventArgs e)
+        {
+            operacion += textBox1.Text + "*";
+            textBox1.Text = "";
+        }
+
+        private void tecladiv_Click(object sender, EventArgs e)
+        {
+            operacion += textBox1.Text + "/";
+            textBox1.Text = "";
+        }
+
+        private void teclaigual_Click(object sender, EventArgs e)
+        {
+            operacion += textBox1;
+            foreach (int i in operacion)
+            {
+
+            }
+        }
+
+        private void teclaCE_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
         }
     }
 }
